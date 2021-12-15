25b08e79ac2d23c2decef8ab9b18a3f803a95000
xXMiroXx <mirooguitar44@gmail.com>
2021-12-15 03:18:03 +0200

    try jpg

--- Components/Hero/Hero.tsx
+++ Components/Hero/Hero.tsx
@@ -8,7 +8,13 @@ const Hero: React.FC = () => {
       <header className={Styles.hero__header}>
         <h1>أعطي شركتك الجنسية البريطانية</h1>
       </header>
-      <main className={Styles.hero__main}>
+      <main
+        // style={{
+        //   backgroundImage:
+        //     "linear-gradient(rgba(100,100,100,.7),rgba(122,122,122,.5)),url(/images/hero/search-bg.webp)",
+        // }}
+        className={Styles.hero__main}
+      >
         <div className={Styles.hero__search}>
           <h2 className={Styles.search__heading}>
             اسم شركتك هو عنوان قصة النجاح
