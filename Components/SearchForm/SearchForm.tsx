69d5232a1f1cffbd0e54632e99e09f66f92f543d
xXMiroXx <mirooguitar44@gmail.com>
2021-12-15 04:30:30 +0200

    add api

--- Components/SearchForm/SearchForm.tsx
+++ Components/SearchForm/SearchForm.tsx
@@ -1,22 +1,41 @@
-import Form from "../UI/Form/Form";
 import useForm from "../../hooks/use-form";
 import SearchInput from "../UI/Form/Inputs/SearchInput";
 import Styles from "./SearchForm.module.scss";
+import { FormEvent } from "react";
+
 const SearchForm: React.FC = () => {
   const [state, inputHandler, getInputField] = useForm([
     { id: "search", note: "", status: false, value: "" },
   ]);
   const searchField = getInputField("search");
+  const submitHandler = (e: FormEvent) => {
+    e.preventDefault();
+    if (!searchField?.status) return;
+  };
   return (
-    <form className={Styles.form}>
+    <form
+      onSubmit={submitHandler}
+      className={`${Styles.form} ${
+        Styles[("form-" + searchField?.status && "valid") || "invalid"]
+      }`}
+    >
       {/* <h3 className={Styles.form__header}></h3> */}
+      <div className={Styles.form__body}>
+        <SearchInput
+          handler={inputHandler("search")}
+          note={searchField?.note || ""}
+          state={searchField?.status || false}
+          value={searchField?.value || ""}
+        />
+      </div>
 
-      <SearchInput
-        handler={inputHandler("search")}
-        note={searchField?.note || ""}
-        state={searchField?.status || false}
-        value={searchField?.value || ""}
-      />
+      <button
+        disabled={!searchField?.status}
+        className={`btn btn-primary ${Styles.form__btn}`}
+        type="submit"
+      >
+        ابحث
+      </button>
     </form>
   );
 };
---
+++ pages/api/search/companies-house.ts
@@ -0,0 +1,48 @@
+import { NextApiHandler } from "next";
+
+const validator: (input: string) => [boolean, string] = (input: string) => {
+  input = input.trim();
+  if (!input) return [false, "ادخل اسم الشركه"];
+  if (input.match(/[^a-zA-Z\s&.,]/))
+    return [false, "اسم الشركه يجب ان يكون بالانجليزيه"];
+  return [true, ""];
+};
+
+async function fetchSearch(name: string) {
+  try {
+    const res = await fetch(process.env.END_POINT + name, {
+      method: "GET",
+      headers: {
+        Authorization: process.env.API_KEY as string,
+      },
+    });
+    return await res.json();
+  } catch (e) {
+    throw e;
+  }
+}
+
+const handler: NextApiHandler = async (req, res) => {
+  try {
+    if (req.method !== "GET") throw new Error("bad request");
+    let { name } = req.query;
+    if (!name) throw new Error("bad url");
+    const [isValid, message] = validator(name as string);
+    if (!isValid) throw new Error(message);
+
+    if (typeof name === "string") {
+      name = name.replace(/ /g, "-");
+      name = encodeURIComponent(name);
+    } else throw new Error("query not strong");
+    const data = await fetchSearch(name as string);
+    const available = !data.total_results;
+    res.status(200).json({
+      status: "success",
+      available,
+    });
+  } catch (e) {
+    res.status(405).json({ message: "something went wrong", error: e.message });
+  }
+};
+
+export default handler;
